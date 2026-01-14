5d2905b051991b2a588dc35e431f2502146f151f
akashca-pro <akashca656@gmail.com>
2026-01-14 12:31:48 +0530

    feat : add recursive method of kthsmallest in bst.

--- 09-trees/binarySearchTree.js
+++ 09-trees/binarySearchTree.js
@@ -302,6 +302,31 @@ function kthSmallest(root, k) {
 }
 
 /**
+ *  Recursive version
+ */
+function kthSmallest(root, k) {
+  let count = 0;
+  let result = null;
+
+  function inorder(node) {
+    if (!node || result !== null) return;
+
+    inorder(node.left);
+    count++;
+    if (count === k) {
+      result = node.val;
+      return;
+    }
+
+    inorder(node.right);
+  }
+
+  inorder(root);
+  return result ?? -1;
+}
+
+
+/**
  * Kth Largest Element
  * Reverse inorder: Right → Root → Left
  */
